7e6779cc641f1eb0fb265d0aa736441f5d183b21
mart <ama2000@outlook.com>
2024-10-17 12:36:05 -0700

    finished grade

--- src/grade.js
+++ src/grade.js
@@ -1,18 +1,34 @@
-const { IndividualClass } = require(".");
 
 class Grade{
-contructor(IndividualClass, graduationYear){
-this.IndividualClass = IndividualClass,
+constructor(individualClasses, graduationYear){
+this.individualClasses = individualClasses,
 this.graduationYear =  graduationYear
 }
 
-getStudentCount(){
+studentCount(){
     let totalStudents = 0
-    for (let i = 0; i < this.IndividualClass.length; i++) {
-    totalStudents += this.IndividualClass[i].student.length 
+    for (let i = 0; i < this.individualClasses.length; i++) {
+    totalStudents += this.individualClasses[i].student.length 
     }
-    return this.totalStudents;
+    return totalStudents;
 } 
+
+calculateGradeAverage(){
+    let sum = 0;
+    for (let i = 0; i < this.individualClasses.length; i++) {
+        for (let j = 0; j < this.individualClasses[i].student.length; j++) {
+            sum += this.individualClasses[i].student[j].calculateTestAverages()
+        }
+    }
+    return sum/this.studentCount();
+}
+
+addIndividualClass(newIndividualClass) {
+    this.individualClasses.push(newIndividualClass)
+}
+
+
+
 //calculateGradeAverage() which returns the average of all test scores for all `Student`s in the `Grade`
 //addIndividualClass()
 
--- test/grade.test.js
+++ test/grade.test.js
@@ -4,13 +4,36 @@ const Student = require('../src').Student;
 const Teacher = require('../src').Teacher;
 
 describe('Grade', () => {
-    beforeEach(const student = new Student('Bob Jones', 19, 12, [60, 70, 80, 90], 2025, ['foosball', 'beerpong', 'basketweaving']);
-    const teacher = new Teacher('Jane Doe', 'Math', 'UCLA', 6);
-    const individualClass = new IndividualClass('Math', 12, teacher, [student]);))
+    let grade;
+    let individualClass;
+    let secondIndividualClass;
+    let student;
+    let teacher;
 
+    beforeEach(function () {
+        student = new Student('Bob Jones', 19, 12, [10, 10, 10, 10], 2025, ['foosball', 'beerpong', 'basketweaving']);
+        teacher = new Teacher('Jane Doe', 'Math', 'UCLA', 6);
+        individualClass = new IndividualClass('Math', 12, teacher, [student]);
+        secondIndividualClass = new IndividualClass('English', 10, teacher, [student]);
+        grade = new Grade([individualClass, secondIndividualClass], 2022);
+    });
     describe('insantiates a new grade', () => {
-        test('takes the following arguments: getStudentCount,calculateGradeAverage(), addIndividualClass() ', () => {
-           
+        test('takes the following arguments: individualClasses and graduationYear', () => {
+            expect(grade.individualClasses).toEqual([individualClass, secondIndividualClass])
+            expect(grade.graduationYear).toBe(2022)
         })
     })
- }
+    describe('insantiates a new grade', () => {
+        test('returns the student count', () => {
+            expect(grade.studentCount()).toEqual(2)
+        })
+    })
+        test('returns the average test grade', () => {
+            expect(grade.calculateGradeAverage()).toEqual(10)
+        })
+        test('adds an individual class to the grade', () => {
+            let thirdIndividualClass = new IndividualClass('Chinese', 10, teacher, [student]);
+            grade.addIndividualClass(thirdIndividualClass);
+            expect(grade.individualClasses).toEqual([individualClass, secondIndividualClass, thirdIndividualClass]);
+        })
+ })
