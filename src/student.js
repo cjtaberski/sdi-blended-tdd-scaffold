791ca19cb565347b80a9ef73bc2ef14bf24d5112
Connor <connorjtaberski@gmail.com>
2024-10-17 12:04:28 -0400

    Added all student tests and methods

--- src/student.js
+++ src/student.js
@@ -1,21 +1,33 @@
 class Student{
- 
-    constructor(name, age, gradeLevel, testScores, graduationYear, activities) {
+    constructor(name, age, gradeLevel, testScores, graduateYear, activities) {
         this.name = name,
         this.age = age,
         this.gradeLevel = gradeLevel,
         this.testScores = testScores,
-        this.graduationYear = graduationYear,
-        this.activities =  activities,
+        this.graduationYear = graduateYear,
+        this.activities =  activities
     }
     updateStudentName(newName) {this.name = newName};
-    uincrementAge(newAge) {this.age = newAge};
-    incrementGradeLevel(newGrade) {this.gradeLevel = newGrade};
-    addTestScore(newTestScores){this.testScores = newTestScores};
-    calculateTestAverages()
+    incrementAge() {this.age++};
+    incrementGradeLevel() {this.gradeLevel++};
+    addTestScore(newTestScores){this.testScores.push(newTestScores)};
+    calculateTestAverages() {
+        let sum = 0;
+        this.testScores.forEach(element => {
+            sum += element
+    })
+        return sum / this.testScores.length;
+    }
     changeGraduationYear(newGraYear) {this.graduationYear = newGraYear};
-    addActivity(newActivity){this.activities = newActivity};
-    removeActivity()
+    addActivity(newActivity){this.activities.push(newActivity)};
+    removeActivity(activityToBeRemoved) {
+        for (let i = 0; i < this.activities.length; i++) {
+            if (this.activities[i] = activityToBeRemoved) {
+                this.activities.pop(this.activities[i])
+            }
+        }
+        return this.activities;
+    }
   }
   
   module.exports = Student;
--- test/student.test.js
+++ test/student.test.js
@@ -3,13 +3,52 @@ const Student = require('../src').Student;
 describe('Student', () => {
     describe('insantiates a new Student', () => {
         test('takes the following arguments: name, age, gradeLevel, testScores, graduationYear, activities', () => {
-    const student = new Student('Jane Doe', 'Biology', 'UCLA', 6, , );
-    expect(student.name).toBe('Jane Doe');
-    expect(student.age).toBe('Biology');
-    expect(student.gradeLevel).toBe('UCLA');
-    expect(student.testScores).toBe(6);
-    expect(student.gradutionYear).toBe(6);
-    expect(student.activities).toBe(6);
-        }
-    }
-}
+            const student = new Student('Bob Jones', 19, 12, [60, 70, 80, 90], 2025, ['foosball', 'beerpong', 'basketweaving']);
+            expect(student.name).toBe('Bob Jones');
+            expect(student.age).toEqual(19);
+            expect(student.gradeLevel).toEqual(12);
+            expect(student.testScores).toEqual([60, 70, 80, 90]);
+            expect(student.graduationYear).toEqual(2025);
+            expect(student.activities).toEqual(['foosball', 'beerpong', 'basketweaving']);
+        })
+        test('Should update the student\'s name', () => {
+            const student = new Student('Bob Jones', 19, 12, [60, 70, 80, 90], 2025, ['foosball', 'beerpong', 'basketweaving']);
+            student.updateStudentName('Robert Jonas');
+            expect(student.name).toBe('Robert Jonas')
+        })
+        test('Should increment the student\'s age by 1', () => {
+            const student = new Student('Bob Jones', 19, 12, [60, 70, 80, 90], 2025, ['foosball', 'beerpong', 'basketweaving']);
+            student.incrementAge();
+            expect(student.age).toEqual(20);
+        })
+        test('Should increment the student\'s grade level by 1', () => {
+            const student = new Student('Bob Jones', 19, 12, [60, 70, 80, 90], 2025, ['foosball', 'beerpong', 'basketweaving']);
+            student.incrementGradeLevel();
+            expect(student.gradeLevel).toEqual(13);
+        })
+        test('Should add the test score to the array', () => {
+            const student = new Student('Bob Jones', 19, 12, [60, 70, 80, 90], 2025, ['foosball', 'beerpong', 'basketweaving']);
+            student.addTestScore(100);
+            expect(student.testScores).toEqual([60, 70, 80, 90, 100]);
+        })
+        test('Should calculate the average of all test scores', () => {
+            const student = new Student('Bob Jones', 19, 12, [60, 70, 80, 90], 2025, ['foosball', 'beerpong', 'basketweaving']);
+            expect(student.calculateTestAverages()).toEqual(75);
+        })
+        test('Should change the student\'s graduation year to the input', () => {
+            const student = new Student('Bob Jones', 19, 12, [60, 70, 80, 90], 2025, ['foosball', 'beerpong', 'basketweaving']);
+            student.changeGraduationYear(2028);
+            expect(student.graduationYear).toEqual(2028);
+        })
+        test('Should add the input to the activity array', () => {
+            const student = new Student('Bob Jones', 19, 12, [60, 70, 80, 90], 2025, ['foosball', 'beerpong', 'basketweaving']);
+            student.addActivity('Hoodratstuff');
+            expect(student.activities).toEqual(['foosball', 'beerpong', 'basketweaving', 'Hoodratstuff']);
+        })
+        test('Should remove the input activity from the activities array', () => {
+            const student = new Student('Bob Jones', 19, 12, [60, 70, 80, 90], 2025, ['foosball', 'beerpong', 'basketweaving']);
+            student.removeActivity('basketweaving');
+            expect(student.activities).toEqual['foosball', 'beerpong']
+        })
+    })
+})
